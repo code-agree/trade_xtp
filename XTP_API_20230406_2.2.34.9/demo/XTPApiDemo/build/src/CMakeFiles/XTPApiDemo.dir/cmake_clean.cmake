fe6b9b6eb9da848745421d0179dcb69f0e870ed2
ywj <lineyua66@gmail.com>
2023-06-11 21:42:42 +0800

    add build

---
+++ XTP_API_20230406_2.2.34.9/demo/XTPApiDemo/build/src/CMakeFiles/XTPApiDemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/XTPApiDemo"
+  "../bin/XTPApiDemo.pdb"
+  "CMakeFiles/XTPApiDemo.dir/quote_spi.cpp.o"
+  "CMakeFiles/XTPApiDemo.dir/xtp_api_demo.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/XTPApiDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
