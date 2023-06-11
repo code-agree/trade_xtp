1f8d6454162a053c70b9b21e28819caa1e0430c6
yuwenjun <lineyua66@gmail.com>
2023-06-11 18:40:02 +0800

    更换last_price的类型

--- XTP_API_20230406_2.2.34.9/demo/XTPApiDemo/api/xquote_api_struct.h
+++ XTP_API_20230406_2.2.34.9/demo/XTPApiDemo/api/xquote_api_struct.h
@@ -19,7 +19,8 @@ typedef struct XTPSpecificTickerStruct
     XTP_EXCHANGE_TYPE exchange_id;
     ///合约代码（不包含交易所信息）例如"600000"，不带空格，以'\0'结尾
 	char	ticker[XTP_TICKER_LEN];
-    std::vector<double> last_price;
+    //std::vector<double> last_price;
+    double last_price;
     std::vector<int64_t> data_time;
 } XTPST;
 
